6e81f5e0c9b8d3df03272b0efc6755902b7696db
roshankhanxox <64058135+roshankhanxox@users.noreply.github.com>
2024-09-08 22:24:15 +0530

    Update settings.py

--- alumnexus/settings.py
+++ alumnexus/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".vercel.app","alumnexusfinal-production.up.railway.app",]
 
 
 # Application definition
